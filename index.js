f32afbf7b3c53d2976e23cb1d8ab625a335bbfac
Zach Stack <stackz.personal@gmail.com>
2019-12-17 12:41:45 -0800

    Complete processLastItem

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
+function processLength(list, callback) {
+ return callback(list.length)
   /* CODE HERE */
 }
 
@@ -66,7 +67,8 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+function processLastItem(stringList, callback) {
+  return callback(stringList.slice (- 1)[0]);
   /* CODE HERE */
 }
 
